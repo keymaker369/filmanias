5fbe2fbe75db19b310166069a6a97c9c239b0965
seke.nenad <seke.nenad@a600039f-2837-9319-d72f-ebaab7387086>
2012-03-12 15:14:50 +0000

    removing. refactoring.

--- trunk/filmanias/src/test/generated/java/org/seke/filmanias/serviceapi/CommentServiceTestBase.java
+++
@@ -1,9 +0,0 @@
-package org.seke.filmanias.serviceapi;
-
-
-/**
- * Definition of test methods to implement.
- */
-public interface CommentServiceTestBase {
-    public void testSaveComment() throws Exception;
-}
--- trunk/filmanias/src/test/generated/java/org/seke/filmanias/serviceapi/RoleServiceTestBase.java
+++
@@ -1,9 +0,0 @@
-package org.seke.filmanias.serviceapi;
-
-
-/**
- * Definition of test methods to implement.
- */
-public interface RoleServiceTestBase {
-    public void testRetrieveAll() throws Exception;
-}
